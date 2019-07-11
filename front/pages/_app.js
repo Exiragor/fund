397aa783357b7105aa88d1f41fd4fcea09f4e0ff
Alexander Lebedev <exiragor@yandex.ru>
2019-07-11 22:30:06 +0300

    add css

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,17 +1,18 @@
-import App, { Container } from 'next/app'
-import React from 'react'
-import { initializeStore } from '../store'
-import { Provider } from 'mobx-react'
+import App, { Container } from 'next/app';
+import React from 'react';
+import { initializeStore } from '../store';
+import { Provider } from 'mobx-react';
+import "../style.css";
 
 class MyMobxApp extends App {
   static async getInitialProps(appContext) {
     // Get or Create the store with `undefined` as initialState
     // This allows you to set a custom default initialState
-    const mobxStore = initializeStore()
+    const mobxStore = initializeStore();
     // Provide the store to getInitialProps of pages
-    appContext.ctx.mobxStore = mobxStore
+    appContext.ctx.mobxStore = mobxStore;
 
-    let appProps = await App.getInitialProps(appContext)
+    let appProps = await App.getInitialProps(appContext);
 
     return {
       ...appProps,
@@ -21,14 +22,14 @@ class MyMobxApp extends App {
 
   constructor(props) {
     super(props)
-    const isServer = typeof window === 'undefined'
+    const isServer = typeof window === 'undefined';
     this.mobxStore = isServer
       ? props.initialMobxState
-      : initializeStore(props.initialMobxState)
+      : initializeStore(props.initialMobxState);
   }
 
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps } = this.props;
     return (
       <Container>
         <Provider store={this.mobxStore}>
