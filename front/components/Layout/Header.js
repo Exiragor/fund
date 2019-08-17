6a4ce7153a92988889632bd151284aa46112c96f
Alexander Lebedev <exiragor@yandex.ru>
2019-08-17 18:33:53 +0300

    add inner page

--- front/components/Layout/Header.js
+++ front/components/Layout/Header.js
@@ -37,39 +37,24 @@ const Header = () => <header>
                         <ul className="sub-menu">
                             <li id="menu-item-124"
                                 className="menu-item menu-item-type-post_type menu-item-object-page menu-item-124">
-                                <Link prefetch href="/other"><a>О фонде</a></Link></li>
+                                <Link prefetch href="/about/fond"><a>О фонде</a></Link></li>
                             <li id="menu-item-123"
                                 className="menu-item menu-item-type-post_type menu-item-object-page menu-item-123">
                                 <Link prefetch href="/other"><a>Как помочь</a></Link></li>
                             <li id="menu-item-122"
                                 className="menu-item menu-item-type-post_type menu-item-object-page menu-item-122">
-                                <Link prefetch href="/other"><a>Отзывы</a></Link></li>
+                                <Link prefetch href="/other"><a>Попечительский совет</a></Link></li>
                             <li id="menu-item-121"
                                 className="menu-item menu-item-type-post_type menu-item-object-page menu-item-121">
                                 <Link prefetch href="/other"><a>Партнеры</a></Link></li>
                             <li id="menu-item-120"
                                 className="menu-item menu-item-type-post_type menu-item-object-page menu-item-120">
-                                <Link prefetch href="/other"><a>Экспертная работа</a></Link></li>
+                                <Link prefetch href="/other"><a>Достижения</a></Link></li>
                             <li id="menu-item-119"
                                 className="menu-item menu-item-type-post_type menu-item-object-page menu-item-119">
-                                <Link prefetch href="/other"><a>Сотрудники</a></Link></li>
+                                <Link prefetch href="/other"><a>Отчеты</a></Link></li>
                         </ul>
                     </li>
-                    <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-96">
-                        <Link prefetch href="/other"><a>Программы</a></Link>
-                    </li>
-                    <li id="menu-item-93"
-                        className="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-93">
-                        <Link prefetch href="/other"><a>Услуги</a></Link>
-                    </li>
-                    <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-90">
-                        <Link prefetch href="/other"><a>Проекты</a></Link>
-                    </li>
-                    <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-103">
-                        <Link prefetch href="/other"><a>Новости</a></Link>
-                    </li>
-                    <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-102">
-                        <Link prefetch href="/other"><a>Контакты</a></Link></li>
                 </ul>
             </div>
         </div>
