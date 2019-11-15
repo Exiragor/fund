9dde63ede9fd5917e65e5076dc318f2044685196
Alexander Lebedev <exiragor@yandex.ru>
2019-11-15 22:17:16 +0300

    change idea of ip

--- adminfront/helpers/index.js
+++ adminfront/helpers/index.js
@@ -1,4 +1,4 @@
-const host = process.env.NODE_ENV == 'production' ? 'http://fundapi.exiragor.pw' : 'http://localhost:8000';
+const host = 'http://localhost:8000';
 export const gethost = () => host;
 export const getImgUrlById = (id) => host + `/files/${id}`;
 export const getImgUrlByIdWithParams = (id, {width, height}) => host + `/files/${id}/${width}/${height}`;
