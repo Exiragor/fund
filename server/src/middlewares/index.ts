f61ce38c1fb496347574128be8829b64b5490991
Alexander Lebedev <exiragor@yandex.ru>
2019-11-15 23:44:12 +0300

    try to fix

--- server/src/middlewares/index.ts
+++ server/src/middlewares/index.ts
@@ -4,6 +4,7 @@ export const allowCrossDomain = (req: Request, res: Response, next: NextFunction
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', '*');
+    res.header('');
 
     next();
 };
