15d3d5d140e0247b593e1f9cc89a2dcb9e875bd5
Alexander Lebedev <exiragor@yandex.ru>
2019-11-16 12:35:29 +0300

    fix name for api, cuz adblock

--- adminfront/helpers/index.js
+++ adminfront/helpers/index.js
@@ -1,4 +1,4 @@
-const host = 'http://fundapi.exiragor.pw';
+const host = 'http://fund.exiragor.pw';
 export const gethost = () => host;
 export const getImgUrlById = (id) => host + `/files/${id}`;
 export const getImgUrlByIdWithParams = (id, {width, height}) => host + `/files/${id}/${width}/${height}`;
--- front/helpers/index.js
+++ front/helpers/index.js
@@ -14,7 +14,7 @@ dateformat.i18n = {
 };
 dateformat.masks.default = "dd mmmm  yyyy";
 
-const host = 'http://fundapi.exiragor.pw';
+const host = 'http://fund.exiragor.pw';
 export const gethost = () => host;
 export const getImgUrlById = (id) => host + `/files/${id}`;
 export const getImgUrlByIdWithParams = (id, {width, height}) => host + `/files/${id}/${width}/${height}`;
