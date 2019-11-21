78ef76dd27df73afe0ff4cea6e38371dabc3a5cb
Alexander Lebedev <exiragor@yandex.ru>
2019-11-21 20:59:11 +0300

    fix

--- adminfront/helpers/index.js
+++ adminfront/helpers/index.js
@@ -1,4 +1,4 @@
-const host = process.env.API_URL || process.env.apiUrl || 'http://localhost:8000';
+const host = process.env.API_URL || process.env.apiUrl || 'http://server.xn--80aap3adkp6cxd.xn--p1ai';
 export const gethost = () => host;
 export const getImgUrlById = (id) => host + `/files/${id}`;
 export const getImgUrlByIdWithParams = (id, {width, height}) => host + `/files/${id}/${width}/${height}`;
