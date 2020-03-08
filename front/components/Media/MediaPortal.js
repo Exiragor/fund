f5ed5501f343a0ab857c73f02f441053624f0ea6
Alexander Lebedev <exiragor@yandex.ru>
2020-03-08 17:13:31 +0300

    naming fix

--- front/components/Media/MediaPortal.js
+++ front/components/Media/MediaPortal.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import ReactDOM from "react-dom";
-import MediaPlayer from "./Mediaplayer";
+import MediaPlayer from "./MediaPlayer";
 
 const MediaPortal = ({ media }) => {
     const
