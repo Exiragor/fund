4abb814880fa83af5a04830d18fa02a6c18771b6
Alexander Lebedev <exiragor@yandex.ru>
2021-02-05 20:43:21 +0300

    dev: change yandex payment urls

--- server/src/services/payment.ts
+++ server/src/services/payment.ts
@@ -1,6 +1,11 @@
 import env from "dotenv";
 env.config();
-const yandexCheckout = require('yandex-checkout')(process.env.YAN_SHOP_ID, process.env.YAN_SECRET_KEY);
+const yandexCheckout = require('yandex-checkout')({
+  shopId: process.env.YAN_SHOP_ID,
+  secretKey: process.env.YAN_SECRET_KEY,
+  host: 'https://api.yookassa.ru',
+  path: '/v3'
+});
 
 export const createPayment = async (amount: number) => {
   var idempotenceKey = 'f0d67c2ed5a5-' + Date.now();
