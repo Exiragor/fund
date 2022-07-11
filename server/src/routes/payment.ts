4eb4e400f99734956294932c75ed4cb892857782
Alexander Lebedev <exiragor@yandex.ru>
2022-07-11 18:48:55 +0300

    feat(server/payment): return error on create payment

--- server/src/routes/payment.ts
+++ server/src/routes/payment.ts
@@ -4,7 +4,12 @@ import {createPayment, getPayment} from '../services/payment';
 export const paymentRouter: Router = Router();
 
 paymentRouter.get('/create', async (req: Request, res: Response) => {
-  res.json(await createPayment(req.query.amount));
+  try {
+    res.json(await createPayment(req.query.amount));
+  } catch (err) {
+    console.error(err);
+    res.json({ error: err });
+  }
 });
 
 paymentRouter.get('/info', async (req: Request, res: Response) => {
