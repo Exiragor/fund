442e05392bbb439b2aa2d30a558e72782b50ceae
Alexander Lebedev <exiragor@yandex.ru>
2022-07-11 19:15:36 +0300

    feat(server/payment): try new payment

--- server/src/services/payment.ts
+++ server/src/services/payment.ts
@@ -1,11 +1,15 @@
 import env from "dotenv";
+import { YooCheckout } from '@a2seven/yoo-checkout'
+
 env.config();
-const yandexCheckout = require('yandex-checkout')({
-  shopId: process.env.YAN_SHOP_ID,
-  secretKey: process.env.YAN_SECRET_KEY,
-  host: 'https://api.yookassa.ru',
-  path: '/v3'
-});
+// const yandexCheckout = require('yandex-checkout')({
+//   shopId: process.env.YAN_SHOP_ID,
+//   secretKey: process.env.YAN_SECRET_KEY,
+//   host: 'https://api.yookassa.ru',
+//   path: '/v3'
+// });
+
+const yandexCheckout = new YooCheckout({ shopId: process.env.YAN_SHOP_ID, secretKey:  process.env.YAN_SECRET_KEY });
 
 export const createPayment = async (amount: number) => {
   var idempotenceKey = 'f0d67c2ed5a5-' + Date.now();
